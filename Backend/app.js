616ff2792ba23105b5edcb118a411dd74effa596
Akhand Pratap Singh <akhand1203@gamil.com>
2026-03-15 01:30:26 +0530

    user resistration done

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectTodb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 connectTodb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
 
@@ -15,5 +18,7 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.send("hello world");
 });
+
+app.use('/users', userRoutes);
 
 module.exports=app;
