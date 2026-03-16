ac152de3f2bbc0399e0860d65f9aeff80e239125
Akhand Pratap Singh <akhand1203@gamil.com>
2026-03-16 16:38:37 +0530

    frontend login and signup page done

---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const UserLogin = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [userData, setUserData] = useState({})
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setUserData({ 
+      email:email,
+       password:password })
+    setEmail('')
+    setPassword('')
+  
+  }
+  return (
+    <div className='p-7 flex flex-col justify-between h-screen'>
+         <div>
+          <img className='w-15 mb-3' src="https://imgs.search.brave.com/Qytw_NXKyFxwwc0vzLr3hbi8hrXtzDbeh_Ziku74uSI/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9sb2dv/cy13b3JsZC5uZXQv/d3AtY29udGVudC91/cGxvYWRzLzIwMjAv/MDUvVWJlci1Mb2dv/LTcwMHgzOTQucG5n" alt="" />
+        <form onSubmit={(e)=>submitHandler(e)} >
+             <h3 className='text-lg font-medium mb-2'>What's your Email?</h3>
+             <input 
+             value={email}
+             onChange={(e)=>{
+              setEmail(e.target.value)
+             }}
+             className='bg-[#eeeeee] mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base'
+             required 
+             type="email" placeholder='email@example.com'
+              />
+             <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+             <input 
+             value={password}
+             onChange={(e)=>{
+             setPassword(e.target.value)
+             }}
+             className='bg-[#eeeeee] mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base'
+             required 
+             type="password" placeholder='password' 
+             />
+             <button
+              className='bg-[#111] text-white  font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base'
+             >Login
+             </button>
+            <p className='text-center'> New here?<Link to="/signup" className='text-blue-600 '>Create new Account</Link></p>
+
+        </form>
+         </div>
+         <div>
+          <Link to="/captain-login" className='bg-[#10b461] text-white flex items-center justify-center font-semibold mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base'>
+            Sign in as Captain
+          </Link>
+         </div>
+    </div>
+  )
+}
+
+export default UserLogin
