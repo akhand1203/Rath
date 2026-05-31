dfbd4e8f14e6433baa18dcfdcd51af05462f1e50
Akhand Pratap Singh <akhand1203@gmail.com>
2026-05-31 19:14:57 +0530

    fix: add vercel URL to CORS

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ console.log('   PORT:', process.env.PORT || 4000);
 connectTodb();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://rath-kappa.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
